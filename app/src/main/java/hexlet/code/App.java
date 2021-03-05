dda66c81dab368bd683554166a438585cfb5d5ad
N Che <error404ruby@gmail.com>
2021-03-05 11:05:28 +0400

    add Makefile, checkstyle.xml, /.github/workflows/main.yml

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,10 +4,10 @@ package hexlet.code;
  * Welcome to the Brain Games!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Welcome to the Brain Games!");
+public class App {
+
+    public static void main(final String[] args) {
+        Cli.hello();
     }
+
 }
