febf6ae446d2b2a22819bac9d3fc51c0c5e5fccb
N Che <error404ruby@gmail.com>
2021-03-10 22:24:48 +0400

    change exit after game

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -16,7 +16,7 @@ public class Engine {
         final int yesANSWERS = 3;
         if (count == yesANSWERS) {
             System.out.println("Congratulations, " + NAME_GAMER + "!");
-            System.exit(0);
+            App.makeGame();
         } else {
             askQuestion(expression);
             getAnswer();
