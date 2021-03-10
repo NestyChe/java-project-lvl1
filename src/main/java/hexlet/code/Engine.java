3524358e9c9ba293760632dc843ed6df5078ec2d
N Che <error404ruby@gmail.com>
2021-03-10 15:32:07 +0400

    make games/Engine.java

---
+++ src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,49 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Engine {
+    private static int count = 0;
+    private static String yourAnswer;
+    private static final String NAME_GAMER = Cli.getName();
+
+    public static int randomInt() {
+        final int min = 1;
+        final int max = 360;
+        return min + (int) (Math.random() * (max - min));
+    }
+
+    public static void playCycle(final String expression, final String result) {
+        final int yesANSWERS = 3;
+        if (count == yesANSWERS) {
+            System.out.println("Congratulations, " + NAME_GAMER + "!");
+            System.exit(0);
+        } else {
+            askQuestion(expression);
+            getAnswer();
+            correctOrNot(result, yourAnswer);
+        }
+    }
+
+
+    public static void askQuestion(final String expression) {
+        System.out.println("Question: " + expression);
+    }
+
+    public static void getAnswer() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Your answer: ");
+        yourAnswer = sc.nextLine();
+    }
+
+    public static void correctOrNot(final String result, final String answer) {
+            if (result.equals(answer)) {
+                System.out.println("Correct!");
+                count += 1;
+            } else {
+                System.out.println(answer + " is wrong answer ;(."
+                + " Correct answer was " + result
+                + ".\nLet's try again, " + NAME_GAMER);
+                count = 0;
+            }
+    }
+}
