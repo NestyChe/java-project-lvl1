38ff5ea3562989dbd4540eba063130ed5ad34698
N Che <error404ruby@gmail.com>
2021-03-10 16:14:44 +0400

    add Calc game in Calc.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
 import java.util.Scanner;
 /**
  * Welcome to the Brain Games!
@@ -13,8 +14,9 @@ public class App {
 
     public static void makeGame() {
         System.out.println("Please enter the game number and press Enter.\n"
-        +  "1 - Greet\n"
+        + "1 - Greet\n"
         + "2 - Even\n"
+        + "3 - Calc\n"
         + "0 - Exit\n");
         System.out.print("Your choice: ");
         Scanner sc = new Scanner(System.in);
@@ -25,6 +27,9 @@ public class App {
                 break;
             case "2":
                 Even.play();
+                break;
+            case "3":
+                Calc.play();
                 break;
             default:
                 break;
---
+++ src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,32 @@
+package hexlet.code.games;
+import hexlet.code.Engine;
+
+public class Calc {
+
+    public static void play() {
+        System.out.println("What is the result of the expression?");
+        int result = 1;
+        String str = "+-*-*+--*+*+-";
+        while (true) {
+        int firstOperand = Engine.randomInt();
+        int secondOperand = Engine.randomInt();
+        char operator;
+        operator = str.charAt(1 + (int) (Math.random() * str.length() - 1));
+        String expression = " " + firstOperand  + operator  + secondOperand;
+        switch (operator) {
+        case '-':
+             result = firstOperand - secondOperand;
+             break;
+        case '+':
+             result = firstOperand + secondOperand;
+             break;
+        case '*':
+             result = firstOperand * secondOperand;
+             break;
+        default:
+             break;
+        }
+        Engine.playCycle(expression, String.valueOf(result));
+        }
+    }
+}
