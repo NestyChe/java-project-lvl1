8e077124f11c09eb1da1f6df2842e23e15b710b7
N Che <error404ruby@gmail.com>
2021-03-10 21:23:16 +0400

    fixed calculation of prime number

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -3,16 +3,17 @@ import hexlet.code.Engine;
 
 public class Prime {
 
+
     public static void play() {
         System.out.println("Answer 'yes' if given number is prime."
                          + " Otherwise answer 'no'.");
-        String result = "no";
         while (true) {
+            String result = "yes";
             int simplyNum = Engine.randomInt();
             int driver = 2;
             while (driver < simplyNum) {
                 if (simplyNum % driver == 0) {
-                    result = "yes";
+                    result = "no";
                 }
                 driver += 1;
             }
